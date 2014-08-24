811803435c78ad02e44cb502e314023a4ac63925
Aaron Kosel <a_kosel@yahoo.com>
2014-08-23 22:56:52 -0400

    revamped styling of sortable lists and other buttons. added some color css classes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,9 +15,7 @@ angular.module('decisionApp')
           accept: function(sourceItemHandleScope, destSortableScope) { 
                 return sourceItemHandleScope.itemScope.sortableScope.$id === destSortableScope.$id;
           },
-          containment: '.rankStep',
-          orderChanged: function(event) {
-          }
+          containment: '.rankStep'
       };
 
       $scope.columnStep = 1;
@@ -42,11 +40,11 @@ angular.module('decisionApp')
           $scope.step = localStorageService.get('step') ? Number(localStorageService.get('step')) : 0;
           $scope.steps = localStorageService.get('steps') ? localStorageService.get('steps') : [
 
-                {'title': 'Define the Problem', 'description': 'Take some time on this one. Oftentimes, a problem is poorly defined or identified as a symptom rather than the root cause.', 'data': [], 'multiple': false, 'dependent': false}, 
-                {'title': 'Determine Values', 'description': 'Values are different aspects of a problem that influence the final decision. For example, a choice for a job may be influenced by location, wages, culture, and the others.', 'data': [], 'multiple': true, 'dependent': false}, 
-                {'title': 'Weight Importance of Values', 'description': 'The values may carry different weight as well. Keeping with jobs example, a desire to reduce commute time might be relatively more important than wages. You can use whatever scale you want (i.e. 1-100), but just note that higher numbers indicate that you prefer the value more.', 'data': [], 'multiple': false, 'dependent': true}, 
+                {'title': 'Define the Problem', 'description': 'Take some time on this one. Oftentimes, a problem is poorly defined or identified as a symptom rather than a root cause.', 'data': [], 'multiple': false, 'dependent': false}, 
+                {'title': 'Determine Values', 'description': 'Values are different aspects of a problem that influence the final decision. For example, a choice for a job may be influenced by location, wages, culture, commute time, and the others.', 'data': [], 'multiple': true, 'dependent': false}, 
+                {'title': 'Weight Importance of Values', 'description': 'The values may carry different weight as well. Keeping with jobs example, a desire to reduce commute time might be relatively more important than wages, so commute time should be higher in the list.', 'data': [], 'multiple': false, 'dependent': true}, 
                 {'title': 'Identify Choices', 'description': 'You often have a number of choices at a particular time. Try to identify all of the relevant ones for your problem.', 'data': [], 'multiple': true, 'dependent': false}, 
-                {'title': 'Rate Each Choice', 'description': 'This can be tough. Take determine how you would rate each choice for each particular value. Again, you can use whatever scale you want (i.e. 1-100), but just note that higher numbers indicate that you prefer the value more.', 'data': [], 'multiple': true, 'dependent': true}, 
+                {'title': 'Rate Each Choice', 'description': 'This can be tough. Take some time to think on which choice best fits a value. For a simple example, if the salary for one job is $35k/year and the salary for another is $60k/year, then the $60k/year job should be at the top of the list.', 'data': [], 'multiple': true, 'dependent': true}, 
                 {'title': 'Find Optimal Decision', 'description': '', 'data': [], 'dependent': true, 'multiple': true}
 
           ]; 
