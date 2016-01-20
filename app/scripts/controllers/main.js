b97e2b3aebc2a7a8984a6c2f0203645efeffd8e0
Aaron Kosel <a_kosel@yahoo.com>
2016-01-20 14:03:12 +0900

    Fix some colors finally. Add some more descriptions.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,7 +12,7 @@ angular.module('decisionApp')
 //     localStorageService.clearAll();
 
       $scope.sortOptions = {
-          accept: function(sourceItemHandleScope, destSortableScope) { 
+          accept: function(sourceItemHandleScope, destSortableScope) {
                 return sourceItemHandleScope.itemScope.sortableScope.$id === destSortableScope.$id;
           },
           containment: '.rankStep'
@@ -40,16 +40,16 @@ angular.module('decisionApp')
           $scope.step = localStorageService.get('step') ? Number(localStorageService.get('step')) : 0;
           $scope.steps = localStorageService.get('steps') ? localStorageService.get('steps') : [
 
-                {'title': 'Define the Problem', 'description': 'Take some time on this one. Oftentimes, a problem is poorly defined or identified as a symptom rather than a root cause.', 'data': [], 'multiple': false, 'dependent': false}, 
-                {'title': 'Determine Values', 'description': 'Values are different aspects of a problem that influence the final decision. For example, a choice for a job may be influenced by location, wages, culture, commute time, and the others.', 'data': [], 'multiple': true, 'dependent': false}, 
-                {'title': 'Rank Values', 'description': 'The values may carry different weight as well. Keeping with jobs example, a desire to reduce commute time might be relatively more important than wages, so commute time should be higher in the list.', 'data': [], 'multiple': false, 'dependent': true}, 
-                {'title': 'Identify Choices', 'description': 'You often have a number of choices at a particular time. Try to identify all of the relevant ones for your problem.', 'data': [], 'multiple': true, 'dependent': false}, 
-                {'title': 'Rate Each Choice', 'description': 'This can be tough. Take some time to think on which choice best fits a value. For a simple example, if the salary for one job is $35k/year and the salary for another is $60k/year, then the $60k/year job should be at the top of the list.', 'data': [], 'multiple': true, 'dependent': true}, 
-                {'title': 'Find Optimal Decision', 'description': '', 'data': [], 'dependent': true, 'multiple': true}
+                {'title': 'Define the Problem', 'description': 'Take some time on this one. Oftentimes, a problem is poorly defined or identified as a symptom rather than a root cause.', 'data': [], 'multiple': false, 'dependent': false},
+                {'title': 'Determine Values', 'description': 'Values are different aspects of a problem that influence the final decision. For example, a choice for a job may be influenced by location, wages, culture, commute time, and the others.', 'data': [], 'multiple': true, 'dependent': false},
+                {'title': 'Rank Values', 'description': 'The values may carry different weight as well. Keeping with jobs example, a desire to reduce commute time might be relatively more important than wages, so commute time should be higher in the list.', 'data': [], 'multiple': false, 'dependent': true},
+                {'title': 'Identify Choices', 'description': 'You often have a number of choices at a particular time. Try to identify all of the relevant ones for your problem.', 'data': [], 'multiple': true, 'dependent': false},
+                {'title': 'Rate Each Choice', 'description': 'This can be tough. Take some time to think on which choice best fits a value. For a simple example, if the salary for one job is $35k/year and the salary for another is $60k/year, then the $60k/year job should be at the top of the list.', 'data': [], 'multiple': true, 'dependent': true},
+                {'title': 'Find Optimal Decision', 'description': 'Here is your ranking! You may find that the choice doesn\'t line up with your intuition. This is OK! One added advantage of this process is it forces us to confront the conflict that often exists between what we think should prefer and what we actually do. Keeping with the job example, we may feel that we should not be as concerned about salary, but in truth, we do actually want to be paid more; it can be valuable to identify these preferences. Feel free to go back and adjust things.', 'data': [], 'dependent': true, 'multiple': true}
 
-          ]; 
+          ];
       };
-      
+
 
       $scope.previous = function() {
           // clear out data from current step
@@ -69,11 +69,11 @@ angular.module('decisionApp')
                     return x !== '';
                 });
             }
-            
+
       };
 
       $scope.getWeight = function(array, item) {
-         return array.length - array.indexOf(item); 
+         return array.length - array.indexOf(item);
       };
 
       $scope.next = function(isValid) {
@@ -92,7 +92,7 @@ angular.module('decisionApp')
                     totals[$scope.steps[$scope.rowStep].data.indexOf($scope.steps[$scope.matrixStep].data[j][i])] +=  $scope.getWeight($scope.steps[$scope.matrixStep].data[j], $scope.steps[$scope.matrixStep].data[j][i]) * $scope.getWeight($scope.steps[$scope.columnStep].data, $scope.steps[$scope.columnStep].data[j]);
                   }
                   $scope.steps[$scope.step + 1].data = totals;
-              } 
+              }
           }
 
           // update the data
